446fc4ec8de152e7cb5bd8c941fc38a006d3a776
funwarioisii <kazuyukihashimoto2006@gmail.com>
2024-11-15 01:44:20 +0900

    add skip-page option and put file counts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { readdir } from "node:fs/promises";
 import md2sb from "md2sb";
 import { loadConvertRules, changeBodyStyle } from "./change-body-style.ts";
 import writeToCosense from "./write-to-cosense.ts";
+import { determinePageName } from "./determine-page-name.ts";
 
 async function setup() {
 
@@ -29,6 +30,9 @@ async function setup() {
       take: {
         type: "string",
       },
+      "skip-page": {
+        type: "string",
+      },
     },
     strict: true,
     allowPositionals: true,
@@ -39,6 +43,7 @@ async function setup() {
   const convertRuleFilePath = values["convert-rule-file"];
   const convertRules = await loadConvertRules(convertRuleFilePath);
   const cosenseProject = values["cosense-project"];
+  const skipPagePath = values["skip-page"];
   const cosenseConnectSid = process.env["COSENSE_CONNECT_SID"];
 
   if (cosenseConnectSid === undefined) {
@@ -50,17 +55,23 @@ async function setup() {
   const mdFiles = paths.filter((path) => path.endsWith(".md"));
   const take = values.take ? parseInt(values.take) : undefined;
 
-  return { path, mdFiles, paths, convertRules, cosenseConnectSid, cosenseProject, dryRun, take };
+  return { path, mdFiles, paths, convertRules, cosenseConnectSid, cosenseProject, dryRun, take, skipPagePath };
 }
 
 async function main() {
-  const { path, mdFiles, convertRules, cosenseConnectSid, cosenseProject, dryRun, take } = await setup();
+  const { path, mdFiles, convertRules, cosenseConnectSid, cosenseProject, dryRun, take, skipPagePath } = await setup();
 
   let mdFilesToWrite = mdFiles;
   if (take !== undefined) {
     mdFilesToWrite = mdFiles.slice(0, take);
   }
+  if (skipPagePath !== undefined) {
+    const skipPage = await Bun.file(skipPagePath).text();
+    const skipPageNames = skipPage.split("\n");
+    mdFilesToWrite = mdFilesToWrite.filter((mdFile) => !skipPageNames.includes(determinePageName(mdFile)));
+  }
 
+  console.info(`writes ${mdFilesToWrite.length.toLocaleString()} pages`);
   for (const mdFile of mdFilesToWrite) {
     const text = await Bun.file(path + "/" + mdFile).text();
     const parentPageName = mdFile.split("/").at(-2)?.replace(".md", "");
@@ -69,7 +80,7 @@ async function main() {
     }
     const body = (await md2sb(text)) + "\n" + (parentPageName ? `[${parentPageName}]\n` : "");
     const convertedBody = await changeBodyStyle(body, convertRules);
-    const pageName = mdFile.split("/").at(-1)!.replace(".md", "");
+    const pageName = determinePageName(mdFile);
 
     if (!dryRun) {
       await writeToCosense({ project: cosenseProject, pageName, text: convertedBody }, cosenseConnectSid)
